05bfd4ec40dc66cb0d0f15948e3a1bee1822a423
Arvinsichuan <arvinsc@foxmail.com>
2017-05-31 10:30:35 +0800

    deleted:    Doc/Data Model - Oracle.docx deleted:    Doc/ERD View.docx new file:   Doc/cover db.pdf modified:   Doc/market promotion sys - DB Design.docx new file:   Doc/market promotion sys - DB Design.pdf modified:   Doc/requirement.markdown modified:   Passed Db scripts/0.init users , tablespaces and privilegs.sql modified:   Passed Db scripts/1.table - cash side employees.sql modified:   Passed Db scripts/2.table - cash side roles.sql modified:   Passed Db scripts/3.table - discount rules.sql modified:   Passed Db scripts/4.table - discount goods.sql modified:   Passed Db scripts/7.table - payments.sql modified:   Passed Db scripts/8.table - orders.sql modified:   Passed Db scripts/9.table - presents.sql modified:   Passed Db scripts/A.table - rules_orders.sql modified:   Passed Db scripts/B.table - saled goods.sql

--- Passed Db scripts/0.init users , tablespaces and privilegs.sql
+++ Passed Db scripts/0.init users , tablespaces and privilegs.sql
@@ -1,31 +1,40 @@
 -- create user
+/*
+	ATTENTION:
+			Change passcode if NECCESARY!
+			Passcode should be the same as the programming side.
+*/
 create user C##Promotion identified by arvinsichuan;
 
 -- create tablespace
+/* ATTENTION:
+			Please create the dir:'C:\oracle\DB FILE\'
+			or change the datafile uri to your customized directory.
+*/
 create tablespace promotion
-	datafile 'C:\oracle\DB FILE\promotion.dat' size 50M
-	autoextend on next 5M;
+datafile 'C:\oracle\DB FILE\promotion.dat' size 50M
+autoextend on next 5M;
 -- grant tablespace to user
 alter user c##promotion default tablespace promotion;
 alter user C##PROMOTION quota unlimited on promotion;
 
 -- granting privilegs
-grant 
-	create session, 
+grant
+	create session,
 	create tablespace,
-	alter any index,
-	alter any table,
-	alter any sequence,
-	alter any trigger,
 	create any index,
 	create any procedure,
 	create any table,
-	backup any table,
-	alter any procedure,
 	create any sequence,
 	create any trigger,
 	create any view,
 	create rollback segment,
+	alter any index,
+	alter any table,
+	alter any sequence,
+	alter any trigger,
+	alter any procedure,
+	backup any table,
 	drop any index,
 	drop any procedure,
 	drop any table,
@@ -37,5 +46,3 @@ grant
 	select any table
 to
 	c##promotion;
-
-
